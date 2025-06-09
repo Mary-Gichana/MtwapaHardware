62994da3b2a47ffa086efa0d54c7801388570b15
Mary Monchari Gichana <marygichana95@gmail.com>
2025-06-09 22:36:00 +0300

    sixth commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,10 +10,12 @@ function Products() {
   return (
     <section id="products" className="bg-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-2xl font-semibold text-gray-900 mb-8 text-center">
+        <h2 className="text-2xl font-semibold text-gray-900 mb-2 text-center">
           Our Products
         </h2>
-
+        <p className="text-xl  text-gray-900 mb-2 text-center">
+          Some of our featured products
+        </p>
         <Swiper
           modules={[Pagination, Navigation]}
           spaceBetween={24}
